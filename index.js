09c4ef5c632fc208773f37c086de00670513dd2f
Fahri Abdul Hakim <fahriabdhakim@yahoo.com>
2018-01-16 10:14:50 +0700

    add var result and var name result to myfunction on index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var age = document.getElementById("age")
 var name = document.getElementById("name")
-var result = age.value;
-var nameresult = name.value;
+
 
 function myFunction() {
+  var result = age.value;
+  var nameresult = name.value;
   if (result >= 17) {
     document.getElementById(`demo`).innerHTML = `Anda Sudah Dewasa`
   } else {
     document.getElementById(`demo`).innerHTML = `Anda Belum Dewasa`
   }
+  console.log(result);
   console.log(nameresult);
 }
